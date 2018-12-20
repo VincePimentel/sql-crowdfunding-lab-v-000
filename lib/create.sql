4c9d342d766014da472e613455d2be77a5a4fffc
Vince Andrew Pimentel <contact@vincep.me>
2018-12-20 02:00:13 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -8,9 +8,14 @@ CREATE TABLE projects (
 );
 
 CREATE TABLE users (
-  
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
 
 CREATE TABLE pledges (
-  
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
 );
